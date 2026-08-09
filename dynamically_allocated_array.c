8bb749d4c1f0630b541854da3ec388d36a348ef9
Lại Tuấn Anh <42168740+tuananhlai@users.noreply.github.com>
2026-08-09 00:50:57 +0000

    feat/dynamically-allocated-array: init

---
+++ dynamically_allocated_array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    int count = 3;
+    int *arr = malloc(count * sizeof(int));
+
+    // Doesn't work as expected.
+    // printf("%zu\n", sizeof(arr) / sizeof(int));
+
+    for (size_t i = 0; i < count; i++) {
+        arr[i] = count - i;
+    }
+
+    for (size_t i = 0; i < count; i++) {
+        printf("%d\n", arr[i]);
+    }
+
+    free(arr);
+        
+    printf("%d\n", arr[1]);
+    return 0;
+}
