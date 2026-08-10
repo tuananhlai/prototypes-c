861c1700e73c35a0c38cce5ebd574a481bfabeee
Lại Tuấn Anh <42168740+tuananhlai@users.noreply.github.com>
2026-08-10 11:34:27 +0000

    feat/chap_3_1_sort: add quick_sort

--- modernC_challenges/chap3_1_sort.c
+++ modernC_challenges/chap3_1_sort.c
@@ -2,9 +2,13 @@
 #include <stdlib.h>
 
 void selection_sort(int arr[], size_t n);
+void insertion_sort(int arr[], size_t n);
 void merge(int arr[], size_t lo1, size_t hi1, size_t hi2);
 void merge_sort(int arr[], size_t n);
 void merge_sort_recursive(int arr[], size_t lo, size_t hi);
+void quick_sort(int arr[], size_t n);
+void quick_sort_recursive(int arr[], size_t lo, size_t hi);
+void swap(int arr[], size_t i, size_t j);
 
 int main(int argc, char const* argv[]) {
   // int arr[] = {10, 5, 2, 6, 3, 1, 2, 8};
@@ -16,12 +20,24 @@ int main(int argc, char const* argv[]) {
   // }
 
   int arr2[] = {10, 5, 2, 6, 3, 1, 2, 8};
-  merge_sort(arr2, 8);
+  quick_sort(arr2, 8);
   for (int i = 0; i < 8; i++) {
     printf("%d\n", arr2[i]);
   }
 
   return EXIT_SUCCESS;
+}
+
+void insertion_sort(int arr[], size_t n) {
+  int j, tmp;
+  for (int i = 1; i < n; i++) {
+    j = i;
+
+    while (j > 0 && arr[j] < arr[j - 1]) {
+      swap(arr, j, j - 1);
+      j--;
+    }
+  }
 }
 
 void selection_sort(int arr[], size_t n) {
@@ -33,15 +49,11 @@ void selection_sort(int arr[], size_t n) {
         min_idx = j;
       }
     }
-    int tmp = arr[i];
-    arr[i] = arr[min_idx];
-    arr[min_idx] = tmp;
+    swap(arr, i, min_idx);
   }
 }
 
-void merge_sort(int arr[], size_t n) {
-  merge_sort_recursive(arr, 0, n);
-}
+void merge_sort(int arr[], size_t n) { merge_sort_recursive(arr, 0, n); }
 
 void merge_sort_recursive(int arr[], size_t lo, size_t hi) {
   if (hi <= lo + 1) return;
@@ -79,4 +91,31 @@ void merge(int arr[], size_t lo1, size_t hi1, size_t hi2) {
   for (int i = lo1; i < hi2; i++) {
     arr[i] = sorted_arr[i - lo1];
   }
+}
+
+void quick_sort(int arr[], size_t n) { quick_sort_recursive(arr, 0, n); }
+
+void quick_sort_recursive(int arr[], size_t lo, size_t hi) {
+  if (hi <= lo + 1) return;
+
+  int anchor = arr[lo];
+  int i = lo + 1;
+  for (int j = i; j < hi; j++) {
+    if (arr[j] >= anchor) {
+      continue;
+    }
+
+    swap(arr, i, j);
+    i++;
+  }
+
+  swap(arr, lo, i - 1);
+  quick_sort_recursive(arr, lo, i);
+  quick_sort_recursive(arr, i, hi);
+}
+
+void swap(int arr[], size_t i, size_t j) {
+  int tmp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = tmp;
 }
