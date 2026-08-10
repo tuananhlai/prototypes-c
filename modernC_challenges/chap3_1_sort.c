4e77fc3feee19efb8931dd6405eb32b79739f687
Tuan Anh <42168740+tuananhlai@users.noreply.github.com>
2026-08-10 02:05:36 +0000

    feat: add merge sort

--- modernC_challenges/chap3_1_sort.c
+++ modernC_challenges/chap3_1_sort.c
@@ -2,15 +2,25 @@
 #include <stdlib.h>
 
 void selection_sort(int arr[], size_t n);
+void merge(int arr[], size_t lo1, size_t hi1, size_t hi2);
+void merge_sort(int arr[], size_t n);
+void merge_sort_recursive(int arr[], size_t lo, size_t hi);
 
 int main(int argc, char const* argv[]) {
-  int arr[] = {10, 5, 2, 6, 3, 1, 2, 8};
-  size_t arr_len = sizeof(arr) / sizeof(int);
-  selection_sort(arr, arr_len);
+  // int arr[] = {10, 5, 2, 6, 3, 1, 2, 8};
+  // size_t arr_len = sizeof(arr) / sizeof(int);
+  // selection_sort(arr, arr_len);
 
-  for (int i = 0; i < arr_len; i++) {
-    printf("%d\n", arr[i]);
+  // for (int i = 0; i < arr_len; i++) {
+  //   printf("%d\n", arr[i]);
+  // }
+
+  int arr2[] = {10, 5, 2, 6, 3, 1, 2, 8};
+  merge_sort(arr2, 8);
+  for (int i = 0; i < 8; i++) {
+    printf("%d\n", arr2[i]);
   }
+
   return EXIT_SUCCESS;
 }
 
@@ -26,5 +36,47 @@ void selection_sort(int arr[], size_t n) {
     int tmp = arr[i];
     arr[i] = arr[min_idx];
     arr[min_idx] = tmp;
+  }
+}
+
+void merge_sort(int arr[], size_t n) {
+  merge_sort_recursive(arr, 0, n);
+}
+
+void merge_sort_recursive(int arr[], size_t lo, size_t hi) {
+  if (hi <= lo + 1) return;
+  size_t mid = (lo + hi) / 2;
+  merge_sort_recursive(arr, lo, mid);
+  merge_sort_recursive(arr, mid, hi);
+  size_t arr1_len = mid - lo;
+  size_t arr2_len = hi - mid;
+  merge(arr, lo, mid, hi);
+}
+
+void merge(int arr[], size_t lo1, size_t hi1, size_t hi2) {
+  int sorted_arr_len = hi2 - lo1;
+  int sorted_arr[sorted_arr_len];
+
+  int i = lo1, j = hi1, k = 0;
+  while (i < hi1 || j < hi2) {
+    if (i >= hi1) {
+      sorted_arr[k] = arr[j];
+      j++;
+    } else if (j >= hi2) {
+      sorted_arr[k] = arr[i];
+      i++;
+    } else if (arr[j] < arr[i]) {
+      sorted_arr[k] = arr[j];
+      j++;
+    } else {
+      sorted_arr[k] = arr[i];
+      i++;
+    }
+
+    k++;
+  }
+
+  for (int i = lo1; i < hi2; i++) {
+    arr[i] = sorted_arr[i - lo1];
   }
 }
