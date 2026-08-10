526b7a7614afd15933b5a99bc9dfbbf1016d9a29
Lại Tuấn Anh <42168740+tuananhlai@users.noreply.github.com>
2026-08-10 13:10:29 +0000

    feat/chap_3_2_derivative: init

---
+++ modernC_challenges/chap3_2_derivative.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+typedef double (*Function)(double);
+
+typedef struct {
+  Function f;
+} Derivative;
+
+double calculate(Derivative* de, double x) {
+  double h = 1e-5;
+  return (de->f(x + h) - de->f(x - h)) / (2.0 * h);
+}
+
+double square(double x) { return x * x; }
+
+int main(int argc, char const* argv[]) {
+  Derivative derivative = {square};
+  double result = calculate(&derivative, 29);
+  printf("%f\n", result);
+  return 0;
+}
