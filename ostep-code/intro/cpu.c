1255d961b1034028cb334064c4b5c01c067baedb
Lại Tuấn Anh <(.id)+(.login)@users.noreply.github.com>
2026-08-18 15:59:12 +0000

    chore: update .devcontainer/Dockerfile

--- ostep-code/intro/cpu.c
+++ ostep-code/intro/cpu.c
@@ -6,12 +6,12 @@
 int main(int argc, char* argv[]) {
   if (argc != 2) {
     fprintf(stderr, "usage: cpu <string>\n");
-    exit(1);
+    exit(EXIT_FAILURE);
   }
   char* str = argv[1];
-  while (1) {
+  while (true) {
     Spin(1);
     printf("%s\n", str);
   }
-  return 0;
+  return EXIT_SUCCESS;
 }
