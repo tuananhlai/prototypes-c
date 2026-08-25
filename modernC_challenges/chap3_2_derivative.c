15a108cf9a81c9069f16ac6e812c3a5e1b18be3f
Tuan Anh <42168740+tuananhlai@users.noreply.github.com>
2026-08-25 00:35:14 +0000

    chore: add chal5_complex_derivative

--- modernC_challenges/chap3_2_derivative.c
+++ modernC_challenges/chap3_2_derivative.c
@@ -6,7 +6,7 @@ typedef struct {
   Function f;
 } Derivative;
 
-double calculate(Derivative* de, double x) {
+double evaluate(Derivative* de, double x) {
   double h = 1e-5;
   return (de->f(x + h) - de->f(x - h)) / (2.0 * h);
 }
@@ -15,7 +15,7 @@ double square(double x) { return x * x; }
 
 int main() {
   Derivative derivative = {square};
-  double result = calculate(&derivative, 29);
+  double result = evaluate(&derivative, 29);
   printf("%f\n", result);
   return 0;
 }
--- modernC_challenges/chap5_chal5_complex-derivative.c
+++ modernC_challenges/chap5_chal5_complex-derivative.c
@@ -1,12 +1,25 @@
+#include <complex.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <complex.h>
+
+typedef double complex (*Function)(double complex);
+
+typedef struct {
+  Function f;
+} Derivative;
+
+double complex evaluate(Derivative *de, double complex x) {
+  double h = 1e-5;
+  return (de->f(x + h) - de->f(x - h)) / (2.0 * h);
+}
+
+double complex square(double complex x) { return x * x; }
 
 /// Can you extend the derivative (Challenge 2) to the complex domain: that is,
 /// functions that receive and return double complex values?
-int main(void) { 
-  double complex a = 1 + I;
-  double complex b = 1 - I;
-  printf("%f\n", a * b);
-  return EXIT_SUCCESS; 
+int main(void) {
+  Derivative derivative = {square};
+  double complex res = evaluate(&derivative, I);
+  printf("z = %.2f%+.2fi\n", creal(res), cimag(res));
+  return EXIT_SUCCESS;
 }
