06b349c4cb27d9eb620b6e0907e128f6ff0cdec3
Lại Tuấn Anh <(.id)+(.login)@users.noreply.github.com>
2026-09-06 14:01:34 +0000

    feat/chap6-chal6: add simple test code

--- modernC_challenges/chap6_chal6_matrix-multiplication.c
+++ modernC_challenges/chap6_chal6_matrix-multiplication.c
@@ -12,27 +12,18 @@ int32_t dot_product(const int32_t vec1[], const int32_t vec2[],
   return retval;
 }
 
-void matrix_vector_product(size_t m, size_t n, const int32_t (*matrix)[n],
+void matrix_vector_product(size_t m, size_t n, const int32_t matrix[m][n],
                            const int32_t vec[], int32_t out_vec[]) {
   for (size_t i = 0; i < m; i++) {
     out_vec[i] = dot_product(matrix[i], vec, n);
   }
 }
 
-int main(void) {
-  int32_t vec1[] = {1, 2, 3, 4};
-  int32_t vec2[] = {9, 8, 7, 6};
-  printf("%d\n", dot_product(vec1, vec2, sizeof(vec1) / sizeof(vec1[0])));
-
-  int32_t matrix[3][4] = {
-      {1, 2, 3, 4},
-      {5, 6, 7, 8},
-      {9, 10, 11, 12},
-  };
-  int32_t result_vec[3];
-  matrix_vector_product(3, 4, matrix, vec1, result_vec);
-  for (size_t i = 0; i < sizeof(result_vec) / sizeof(result_vec[0]); i++) {
-    printf("%d\n", result_vec[i]);
+void matrix_inverse(size_t m, size_t n, const int32_t matrix[m][n],
+                    int32_t out_matrix[n][m]) {
+  for (size_t i = 0; i < m; i++) {
+    for (size_t j = 0; j < n; j++) {
+      out_matrix[i][j] = -matrix[i][j];
+    }
   }
-  return EXIT_SUCCESS;
 }
---
+++ modernC_challenges/chap6_chal6_matrix-multiplication_test.c
@@ -0,0 +1,59 @@
+#include "../acutest.h"
+
+#define UNIT_TEST
+#include "chap6_chal6_matrix-multiplication.c"
+
+void test_dot_product(void) {
+  int32_t vec1[] = {1, 2, 3, 4};
+  int32_t vec2[] = {9, 8, 7, 6};
+  int32_t result = dot_product(vec1, vec2, 4);
+  TEST_CHECK(result == 70);
+  TEST_MSG("expected 70, got %d", result);
+
+  result = dot_product(vec1, vec1, 0);
+  TEST_CHECK(result == 0);
+  TEST_MSG("expected 0, got %d", result);
+}
+
+void test_matrix_vector_product(void) {
+  int32_t matrix[3][4] = {
+      {1, 2, 3, 4},
+      {5, 6, 7, 8},
+      {9, 10, 11, 12},
+  };
+  int32_t vec[] = {1, 2, 3, 4};
+  int32_t expected[3] = {30, 70, 110};
+  int32_t out[3];
+  matrix_vector_product(3, 4, matrix, vec, out);
+  for (size_t i = 0; i < 3; i++) {
+    TEST_CHECK_(out[i] == expected[i], "out[%zu] == %d (got %d)", i,
+                expected[i], out[i]);
+  }
+}
+
+void test_matrix_inverse(void) {
+  int32_t matrix[2][2] = {
+      {2, 1},
+      {5, 3},
+  };
+  int32_t expected[2][2] = {
+      {3, -1},
+      {-5, 2},
+  };
+  int32_t inversed_matrix[2][2];
+  matrix_inverse(2, 2, matrix, inversed_matrix);
+  for (size_t i = 0; i < 2; i++) {
+    for (size_t j = 0; j < 2; j++) {
+      TEST_CHECK_(inversed_matrix[i][j] == expected[i][j],
+                  "inversed_matrix[%zu][%zu] == %d (got %d)", i, j,
+                  expected[i][j], inversed_matrix[i][j]);
+    }
+  }
+}
+
+TEST_LIST = {
+    {"dot_product", test_dot_product},
+    {"matrix_vector_product", test_matrix_vector_product},
+    {"matrix_inverse", test_matrix_inverse},
+    {NULL, NULL},
+};
